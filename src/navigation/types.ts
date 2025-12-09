227b784592da98f59f88115554be4909e0cf47a2
Precious Adegbaju <82271018+Precious333@users.noreply.github.com>
2025-12-09 14:36:55 +0100

    Delete src/navigation directory

--- src/navigation/types.ts
+++
@@ -1,8 +0,0 @@
-import { InventoryItem } from "../types/inventory";
-
-export type RootStackParamList = {
-  Dashboard: undefined;
-  Inventory: undefined;
-  AddItem: { item?: InventoryItem } | undefined;
-  ItemDetail: { id: string };
-};
