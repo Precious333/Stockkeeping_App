8c058f07702fb4a498f9ee19fb6316a34d6ae0e6
Precious Adegbaju <82271018+Precious333@users.noreply.github.com>
2025-12-09 14:37:33 +0100

    Delete src/utils directory

--- src/utils/format.ts
+++
@@ -1,8 +0,0 @@
-export const formatPrice = (value: number) => {
-  if (!value || isNaN(value)) return "₦0.00";
-
-  return "₦" + Number(value).toLocaleString("en-NG", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
-};
